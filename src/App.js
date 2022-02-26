17c029b8b74713abe51a3cb14c85a97b0eb07509
Mark-Nelovka <87484591+Mark-Nelovka@users.noreply.github.com>
2022-02-26 14:51:46 +0200

    change files App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import Profile from "./Profile/Profile";
 import user from "./Profile/user.json";
+import Profile from "./Profile/Profile";
+import Transaction from "./TransactionsApp";
+import FriendList from "./FriendListApp";
+import Statistics from "./Statistics";
 export default function App() {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics />
+      <FriendList />
+      <Transaction />
+    </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,42 +1,13 @@
-// import { div } from "prelude-ls";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import ProfileApp from "./App";
-// import { doc } from "prettier";
-import StatisticsApp from "./Statistics";
-import FriendListApp from "./FriendListApp";
-import TransactionsApp from "./TransactionsApp";
+import App from "./App";
 
 const div = document.querySelector("#root");
 
 ReactDOM.render(
   <React.StrictMode>
-    <ProfileApp />
-    <StatisticsApp />
-    <FriendListApp />
-    <TransactionsApp />
+    <App />
   </React.StrictMode>,
   div
 );
-
-// ReactDOM.render(, div);
-// ReactDOM.render(<App />, div);
-// import App from './App';
-
-// const elem1 = <span>Hello</span>;
-// const elem2 = <span> world!</span>
-// console.log(div)
-// const element =
-//     <div>
-//         {elem1}
-//         {elem2}
-//     </div>;
-// console.log(element)
-// ReactDOM.render(element, div);
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   div);
